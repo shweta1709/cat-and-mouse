57b504fb0b50c4e2e8ff100656e71945c419baa8
shweta1709 <76553502+shweta1709@users.noreply.github.com>
2021-03-14 11:07:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,73 @@
-var cat  ,catAnimation;
-var mouse, mouseAnimation;
-var garden,gardenAnimation;
+var jerry, jerryAnimation;
+var tom , tomAnimation;
+var cat, catAnimation;
+var garden , gardenImage;
+
 
 
 
 
 function preload() {
     //load the images here
-    catAnimation = loadAnimation("tomOne.png","tomTwo.png","tomThree.png");
 
-    mouseAnimation  = loadAnimation("jerryOne.png","jerryTwo.png","jerryThree.png");
+ jerryAnimation = loadAnimation("images/jerryTwo.png","images/jerryThree.png");
 
-    gardenAnimation = loadAnimation("garden.png");
+ tomAnimation = loadAnimation("images/tomTwo.png","images/tomThree.png");
+
+ catAnimation = loadAnimation("images/tomFour.png");
+
+ mouseAnimation = loadAnimation("images/jerryFour.png");
+
+gardenImage = loadImage("images/garden.png");
 
 }
 
 function setup(){
     createCanvas(1000,800);
-
-    garden = createSprite( 500,400,1000,800);
-    garden.addAnimation("garden.png",gardenAnimation);
-
-    cat = createSprite(500,400,3,2);
-    catAnimation = loadAnimation("tomOne.png","tomTwo.png","tomThree.png");
-    
-    mouse = createSprite(200,150,3,2);
-    mouseAnimation  = loadAnimation("jerryOne.png","jerryTwo.png","jerryThree.png");
     //create tom and jerry sprites here
+  garden = createSprite(500,400,10,80);
+  garden.scale = 1.2;
+  garden.addImage(gardenImage)
+   
+  tom = createSprite(800,600,8,6);
+  tom.addAnimation("tomAnimation",tomAnimation);
+  tom.scale =0.2;
+  tom.velocityX = -3;
+
+
+  jerry = createSprite(100,600,3,2);
+  jerry.addAnimation("jerryAnimation",jerryAnimation);
+  jerry.scale = 0.1;
+
+ jerry.setCollider("rectangle",0,0,width/2,height/2);
+ jerry.debug = false;
+
+tom.setCollider("rectangle",0,0,width+30,height/2);
+ tom.debug = false;
+
 
 }
 
 function draw() {
 
-    background(255);
+    background(0,0,0);
+
+    if(tom.x-jerry.x<(tom.width-jerry.width)/2){
+        tom.addAnimation("images/tomFour.png",catAnimation);
+        tom.changeAnimation("images/tomFour.png");
+        tom.velocityX =0;
+        jerry.addAnimation("images/jerryFour.png",mouseAnimation);
+        jerry.changeAnimation("images/jerryFour.png");
+      
+        }
+
+
+   
+    
     //Write condition here to evalute if tom and jerry collide
 
     drawSprites();
 }
 
 
-function keyPressed(){
 
-  //For moving and changing animation write code here
-
-
-}
